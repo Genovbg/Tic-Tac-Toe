3b11b1efee7ec8e7e918018ad1626b6b0c12b1f9
Stefan Genov <stefan.genov@mail.bg>
2017-04-22 00:59:43 +0300

    Add files via upload

---
+++ TicTacToeServerTest.java
@@ -0,0 +1,13 @@
+package Tic_Tac_Toe;
+
+import javax.swing.JFrame;
+
+public class TicTacToeServerTest {
+public static void main(String[] args) {
+		
+		TicTacToeServer application = new TicTacToeServer();
+		application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		application.execute();
+
+	}
+}
